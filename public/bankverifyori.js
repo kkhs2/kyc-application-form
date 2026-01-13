393de9c79853d48ca4ef3ccf241445c2c5891327
kkhs2 <shumkhk@gmail.com>
2026-01-13 16:24:40 +0000

    initial working version

--- public/bankverifyori.js
+++
@@ -1,9 +0,0 @@
-
-const nextButtons = [...document.getElementsByTagName("next-button")];
-
-nextButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    const errors = window.iterateForErrors();
-    console.log(errors);
-  });
-});
