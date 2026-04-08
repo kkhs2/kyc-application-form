1f7ef2e0284b6648bb18c541b8aa14e75bf821bc
ks <kenneth.shum@booker.co.uk>
2026-04-08 09:25:07 +0100

    Update vddemail.js

--- public/vddemail.js
+++ public/vddemail.js
@@ -1,10 +1,10 @@
 /* VDD email address disable input */
-const inputs = [...document.getElementsByTagName("kyc-input")];
-const email = inputs.find(
-  (i) => i.getAttribute("data-name") === "EmailAddress"
-);
+    const inputs = [...document.getElementsByTagName("kyc-input")];
+    const email = inputs.find(
+        (i) => i.getAttribute("data-name") === "VDDEmailAddress"
+    );
 
-if (email) {
-  const emailInput = email.shadowRoot;
-  emailInput.querySelector('input').disabled = true;
-}
+    if (email) {
+        const emailInput = email.shadowRoot;
+        emailInput.querySelector('input').disabled = true;
+    }
